fdeaae370a88387ba2f254ca5476664ffa43992f
RTX4070TI <evequeenrevenge@gmail.com>
2024-11-20 10:49:25 +0100

    refactor(BackendNote): refactor DataSeeder to avoid duplicate code

--- BackendNote/Data/DataSeeder.cs
+++ BackendNote/Data/DataSeeder.cs
@@ -5,71 +5,27 @@ namespace BackendNote.Data;
 
 public class DataSeeder(NotesService notesService, ILogger<DataSeeder> logger)
 {
-    private readonly List<Note> _notesAtStartup =
-    [
-        new Note
-        {
-            PatientId = 1,
-            Title = "",
-            Body = "Le patient déclare qu'il 'se sent très bien' Poids égal ou inférieur au poids recommandé"
-        },
+    private static Note CreateNote(int patientId, string body) => new() { PatientId = patientId, Title = "", Body = body };
 
-        new Note
-        {
-            PatientId = 2,
-            Title = "",
-            Body = "Le patient déclare qu'il ressent beaucoup de stress au travail Il se plaint également que son audition est anormale dernièrement"
-        },
+    private readonly List<Note> _notesAtStartup = new()
+    {
+        // Patient 1
+        CreateNote(1, "Le patient déclare qu'il 'se sent très bien' Poids égal ou inférieur au poids recommandé"),
 
-        new Note
-        {
-            PatientId = 2,
-            Title = "",
-            Body = "Le patient déclare avoir fait une réaction aux médicaments au cours des 3 derniers mois Il remarque également que son audition continue d'être anormale"
-        },
+        // Patient 2
+        CreateNote(2, "Le patient déclare qu'il ressent beaucoup de stress au travail Il se plaint également que son audition est anormale dernièrement"),
+        CreateNote(2, "Le patient déclare avoir fait une réaction aux médicaments au cours des 3 derniers mois Il remarque également que son audition continue d'être anormale"),
 
-        new Note
-        {
-            PatientId = 3,
-            Title = "",
-            Body = "Le patient déclare qu'il fume depuis peu"
-        },
+        // Patient 3
+        CreateNote(3, "Le patient déclare qu'il fume depuis peu"),
+        CreateNote(3, "Le patient déclare qu'il est fumeur et qu'il a cessé de fumer l'année dernière Il se plaint également de crises d'apnée respiratoire anormales Tests de laboratoire indiquant un taux de cholestérol LDL élevé"),
 
-        new Note
-        {
-            PatientId = 3,
-            Title = "",
-            Body = "Le patient déclare qu'il est fumeur et qu'il a cessé de fumer l'année dernière Il se plaint également de crises d’apnée respiratoire anormales Tests de laboratoire indiquant un taux de cholestérol LDL élevé"
-        },
-
-        new Note
-        {
-            PatientId = 4,
-            Title = "",
-            Body = "Le patient déclare qu'il lui est devenu difficile de monter les escaliers Il se plaint également d’être essoufflé Tests de laboratoire indiquant que les anticorps sont élevés Réaction aux médicaments"
-        },
-
-        new Note
-        {
-            PatientId = 4,
-            Title = "",
-            Body = "Le patient déclare qu'il a mal au dos lorsqu'il reste assis pendant longtemps"
-        },
-
-        new Note
-        {
-            PatientId = 4,
-            Title = "",
-            Body = "Le patient déclare avoir commencé à fumer depuis peu Hémoglobine A1C supérieure au niveau recommandé"
-        },
-
-        new Note
-        {
-            PatientId = 4,
-            Title = "",
-            Body = "Taille, Poids, Cholestérol, Vertige et Réaction"
-        }
-    ];
+        // Patient 4
+        CreateNote(4, "Le patient déclare qu'il lui est devenu difficile de monter les escaliers Il se plaint également d'être essoufflé Tests de laboratoire indiquant que les anticorps sont élevés Réaction aux médicaments"),
+        CreateNote(4, "Le patient déclare qu'il a mal au dos lorsqu'il reste assis pendant longtemps"),
+        CreateNote(4, "Le patient déclare avoir commencé à fumer depuis peu Hémoglobine A1C supérieure au niveau recommandé"),
+        CreateNote(4, "Taille, Poids, Cholestérol, Vertige et Réaction")
+    };
 
     public async Task SeedNotesAsync()
     {

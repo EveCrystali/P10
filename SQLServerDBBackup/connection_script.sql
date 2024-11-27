91bcb656679fb7ff168a290dd04ceee3a4949543
RTX4070TI <evequeenrevenge@gmail.com>
2024-11-27 10:15:12 +0100

    chore(sqlserver): remove unnecessary system logins from sp_help_revlogin script, keeping only backend_user

--- SQLServerDBBackup/connection_script.sql
+++ SQLServerDBBackup/connection_script.sql
@@ -2,19 +2,6 @@
 ** Generated nov 27 2024  9:32AM on RTX4070Ti\SQLEXPRESS
 */
  
-/* Login [AUTORITE NT\Système] */
-IF NOT EXISTS (
-    SELECT 1
-    FROM sys.server_principals
-    WHERE [name] = N'AUTORITE NT\Système'
-    )
-BEGIN
-    CREATE LOGIN [AUTORITE NT\Système]
-    FROM WINDOWS
-    WITH DEFAULT_DATABASE = [master]
-        ,DEFAULT_LANGUAGE = [Français]
-END
- 
 /* Login [backend_user] */
 IF NOT EXISTS (
     SELECT 1
@@ -32,94 +19,4 @@ BEGIN
 
 	
 	EXEC [master].dbo.sp_addsrvrolemember @loginame = N'backend_user', @rolename = N'sysadmin'
-END
- 
-/* Login [BUILTIN\Utilisateurs] */
-IF NOT EXISTS (
-    SELECT 1
-    FROM sys.server_principals
-    WHERE [name] = N'BUILTIN\Utilisateurs'
-    )
-BEGIN
-    CREATE LOGIN [BUILTIN\Utilisateurs]
-    FROM WINDOWS
-    WITH DEFAULT_DATABASE = [master]
-        ,DEFAULT_LANGUAGE = [Français]
-END
- 
-/* Login [NT Service\MSSQL$SQLEXPRESS] */
-IF NOT EXISTS (
-    SELECT 1
-    FROM sys.server_principals
-    WHERE [name] = N'NT Service\MSSQL$SQLEXPRESS'
-    )
-BEGIN
-    CREATE LOGIN [NT Service\MSSQL$SQLEXPRESS]
-    FROM WINDOWS
-    WITH DEFAULT_DATABASE = [master]
-        ,DEFAULT_LANGUAGE = [Français]
-
-	
-	EXEC [master].dbo.sp_addsrvrolemember @loginame = N'NT Service\MSSQL$SQLEXPRESS', @rolename = N'sysadmin'
-END
- 
-/* Login [NT SERVICE\SQLTELEMETRY$SQLEXPRESS] */
-IF NOT EXISTS (
-    SELECT 1
-    FROM sys.server_principals
-    WHERE [name] = N'NT SERVICE\SQLTELEMETRY$SQLEXPRESS'
-    )
-BEGIN
-    CREATE LOGIN [NT SERVICE\SQLTELEMETRY$SQLEXPRESS]
-    FROM WINDOWS
-    WITH DEFAULT_DATABASE = [master]
-        ,DEFAULT_LANGUAGE = [Français]
-END
- 
-/* Login [NT SERVICE\SQLWriter] */
-IF NOT EXISTS (
-    SELECT 1
-    FROM sys.server_principals
-    WHERE [name] = N'NT SERVICE\SQLWriter'
-    )
-BEGIN
-    CREATE LOGIN [NT SERVICE\SQLWriter]
-    FROM WINDOWS
-    WITH DEFAULT_DATABASE = [master]
-        ,DEFAULT_LANGUAGE = [Français]
-
-	
-	EXEC [master].dbo.sp_addsrvrolemember @loginame = N'NT SERVICE\SQLWriter', @rolename = N'sysadmin'
-END
- 
-/* Login [NT SERVICE\Winmgmt] */
-IF NOT EXISTS (
-    SELECT 1
-    FROM sys.server_principals
-    WHERE [name] = N'NT SERVICE\Winmgmt'
-    )
-BEGIN
-    CREATE LOGIN [NT SERVICE\Winmgmt]
-    FROM WINDOWS
-    WITH DEFAULT_DATABASE = [master]
-        ,DEFAULT_LANGUAGE = [Français]
-
-	
-	EXEC [master].dbo.sp_addsrvrolemember @loginame = N'NT SERVICE\Winmgmt', @rolename = N'sysadmin'
-END
- 
-/* Login [RTX4070Ti\antoi] */
-IF NOT EXISTS (
-    SELECT 1
-    FROM sys.server_principals
-    WHERE [name] = N'RTX4070Ti\antoi'
-    )
-BEGIN
-    CREATE LOGIN [RTX4070Ti\antoi]
-    FROM WINDOWS
-    WITH DEFAULT_DATABASE = [master]
-        ,DEFAULT_LANGUAGE = [Français]
-
-	
-	EXEC [master].dbo.sp_addsrvrolemember @loginame = N'RTX4070Ti\antoi', @rolename = N'sysadmin'
 END
